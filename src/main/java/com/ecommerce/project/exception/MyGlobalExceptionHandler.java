8a790fe66a00415bac5e2f6149eb8eba52fbe2cf
Rambala S <rambala5246@gmail.com>
2025-06-13 00:45:18 +0530

    e-com-13/06/2025

--- src/main/java/com/ecommerce/project/exception/MyGlobalExceptionHandler.java
+++ src/main/java/com/ecommerce/project/exception/MyGlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.ecommerce.project.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -12,14 +14,14 @@ import java.util.Map;
 public class MyGlobalExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public Map<String, String> myMethodArgumentNotValid(MethodArgumentNotValidException e) {
+    public ResponseEntity<Map<String, String>> myMethodArgumentNotValid(MethodArgumentNotValidException e) {
         Map<String, String> response = new HashMap<>();
         e.getBindingResult().getAllErrors().forEach(error -> {
             String fieldName = ((FieldError)error).getField();
             String errMsg = error.getDefaultMessage();
             response.put(fieldName, errMsg);
         });
-        return response;
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
 
 }
