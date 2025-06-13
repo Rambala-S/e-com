72646ff54d5ce6a63fa6e0267befea912ff7565b
Rambala S <rambala5246@gmail.com>
2025-06-13 16:48:11 +0530

    Revert "e-com-13/06/2025"
    This reverts commit 0c3d38bc825aede95f73fb166ed4583f740af011.

--- src/main/java/com/ecommerce/project/repo/CategoriesRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ecommerce.project.repo;
-
-import com.ecommerce.project.model.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CategoriesRepository extends JpaRepository<Category, Long> {
-}
